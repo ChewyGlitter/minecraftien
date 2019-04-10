6bd62a9bc32b15c35be2bdb083d480ea625f513c
ChewyGlitter <49228325+ChewyGlitter@users.noreply.github.com>
2019-04-10 11:27:50 +0200

    Update and rename .gitignore/index.js to  .gitignore/index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,32 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "?";
+const client = new Discord. Client();
+var prefix = "!";
  
-client.login('NTEwMzM2NDcyNDE1NjAwNjQx.XKOV1A.IYWJjMaDZIs1pPRXaykT-bp2X68');
- 
-client.on('message', message =>{
-    if(message.content === "tu fais quoi?"){
-        message.channel.sendMessage('Je mange des pâtes dans ma pastabox ! :heart:');
-        console.log('répond à tfq');
+client. login ( 'NTEwMzM2NDcyNDE1NjAwNjQx.XK2t3g.qAQC0P4hSllrmPIzRns06oSLwg8' ) ;
+
+client.on('message', message => {
+    if(message.content === "!Commencer Poudlard") {
+        message.reply('Salut jeune aventurier tu vas maintenant commencer ton aventure est tu prés ?') ;
+        console.log( 'répond au commencements de Poudlard.' ) ;
     }
-});
+} ) ;
+
+client.on('message', message => {
+    if(message.content === "!Commencer Poudlard") {
+        message.reply('Pour continuer fais !Début dans #début !!') ;
+        console.log( 'répond au commencements de Poudlard.' ) ;
+    }
+} ) ;
+
+client.on('message', message => {
+    if(message.content === "Salut") {
+        message.reply('Salut ça va ou pas') ;
+        console.log( 'répond à slt' ) ;
+    }
+} ) ;
+
+client.on("guildMemberAdd", member =>{
+    member.guild.channels.get("513038742727032844").send(":tada: **Bienvenue **" + member.use + ":smile: Nous sommes content que tu est rejoins maintenant nous sommes " + member.guild.memberCount );
+    console.log("+1");
+})
+
